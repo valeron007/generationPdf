faabf379b20c6cf0c8afee0d18f757df251a34d3
valeron <valeronchik0702@gmail.com>
2024-04-02 10:11:11 +0300

    added fill Pdf

---
+++ FillPdf/fillpdf.py
@@ -0,0 +1,51 @@
+from PyPDF4.pdf import PdfFileReader, PdfFileWriter
+from PyPDF4.generic import NameObject, BooleanObject, IndirectObject
+
+from fabricMethod.Act import Act
+import base64
+
+
+class FillPdf:
+    def __init__(self, data: Act):
+        self.writer = PdfFileWriter()
+        self.reader = PdfFileReader("\\template\\main.pdf", strict=False)
+        self.act_data = data
+
+    def fillTemplate(self):
+        self.setUpdateRoot()
+        self.catalog = self.writer._root_object
+        self.setUpdateCatalog()
+        need_appearances = NameObject("/NeedAppearances")
+        self.writer._root_object["/AcroForm"][need_appearances] = BooleanObject(True)
+        self.needAppearances()
+        self.writer.appendPagesFromReader(self.reader)
+        self.writer.updatePageFormFieldValues(self.writer.getPage(0), self.act_data.act_pdf)
+
+    def setUpdateRoot(self):
+        if "/AcroForm" in self.reader.trailer["/Root"]:
+            self.reader.trailer["/Root"]["/AcroForm"].update(
+                {NameObject("/NeedAppearances"): BooleanObject(True)}
+            )
+
+    def setUpdateCatalog(self):
+        if "/AcroForm" not in self.catalog:
+            self.writer._root_object.update({
+                NameObject("/AcroForm"): IndirectObject(len(self.writer._objects), 0, self.writer)})
+
+    def needAppearances(self):
+        need_appearances = NameObject("/NeedAppearances")
+        self.writer._root_object["/AcroForm"][need_appearances] = BooleanObject(True)
+
+        if "/AcroForm" in self.writer._root_object:
+            self.writer._root_object["/AcroForm"].update(
+                {NameObject("/NeedAppearances"): BooleanObject(True)})
+
+    def writePdf(self):
+        with open("1.pdf", "wb") as f:
+            self.writer.write(f)
+
+    def getPdfBaseEncode(self):
+        with open("1.pdf", "rb") as pdfFile:
+            pdf_encode = base64.b64encode(pdfFile.read()).decode('utf-8')
+
+        return pdf_encode
